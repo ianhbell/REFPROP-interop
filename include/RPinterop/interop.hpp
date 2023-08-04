d794653b0af9f2b64fa68fc21b4fa5fca68fd78d
Ian Bell <ian.bell@nist.gov>
2023-08-04 13:41:29 -0400

    Make the copy automatic by passing by value

--- include/RPinterop/interop.hpp
+++ include/RPinterop/interop.hpp
@@ -85,8 +85,7 @@ namespace internal{
         return std::vector<std::string>(lines.begin()+istart, lines.begin()+iend);
     }
 
-    std::string to_upper(const std::string& str){
-        auto s = str; // copy :(
+    std::string to_upper(const std::string s){
         std::locale locale;
         auto f = [&locale] (char ch) { return std::use_facet<std::ctype<char>>(locale).toupper(ch); };
         std::transform(s.begin(), s.end(), s.begin(), f);
